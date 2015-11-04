6307b46b0af47812fb16d51071973ece029cc59d
David Ferrand <david@supenta.com>
2015-11-04 15:02:09 +0000

    Try to use local include instead of flags

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,5 +1,8 @@
 #CFLAGS=-Wall -O0 -g -fPIC
 CFLAGS := -g
-ARDRONE_SDK_PATH := ../../../../ARDroneLib
+ARDRONE_SDK_PATH := ../../../../../ARDroneLib
+ARDRONE_TARGET_BUILD := DEBUG_MODE
+ARDRONE_TARGET_OS := Linux_3.19.0-30-generic_GNU_Linux
+GCC := homedavidAndroidDeveloperToolssdkndk-bundletoolchainsarm-linux-androideabi-4.9prebuiltlinux-x86_64binarm-linux-androideabi-gcc_4.9
 include $(all-subdir-makefiles)
 
